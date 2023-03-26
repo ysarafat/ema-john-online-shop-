4d686f74c80636c5ce562a21c7c47ec1e4099f8e
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-03-27 03:06:25 +0600

    price fixed

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -7,7 +7,7 @@ const Product = (props) => {
         <div className='product-cards'>
             <img src={img} alt={name} />
             <h3 className='product__name'>{name}</h3> 
-            <p className='product__price'>Price : {price}</p>
+            <p className='product__price'>Price : ${price}</p>
             <p className='product__seller'>Seller : {seller}</p>
             <p className='product__seller'>Rating : {ratings}</p>
             <button className='product__cart_btn'>Add to Cart</button>
